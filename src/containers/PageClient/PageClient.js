21bc8819f7d3ac78877113acafe1d27ccda9cfff
Frédéric Pilon <fpilon@pixelcircus.ca>
2020-08-20 09:44:55 -0400

    Page Client redirect si aucun client avec ce nom.

--- src/containers/PageClient/PageClient.js
+++ src/containers/PageClient/PageClient.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React from "react";
 
-//import axios from 'axios';
-//import variables from "./../../variables";
+import axios from "axios";
+import variables from "./../../variables";
 
 const PageClient = (props) => {
-    const {code} = props;
-    
-    return (
-        <div className="Container">Client {code}</div>
-    )
-}
+  const { code } = props;
+  axios
+    .get(variables.phpfolder + "feed.php?clientcheck=" + code)
+    .then((response) => {
+      console.log(response.data.noresults);
+      if (response.data.noresults) {
+        console.log("No client");
+        window.location = "/?codenotfound=" + code;
+      } else {
+        console.log("test");
+        //window.location = "/client/" + code;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return <div className="Container">Client {code}</div>;
+};
 
 export default PageClient;
